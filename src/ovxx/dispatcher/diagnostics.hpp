b0ca6be8e8a0d87b31b180e6184efc1b9e5f09b2
Stefan Seefeld <stefan@seefeld.name>
2020-08-14 22:03:20 -0400

    Remove pre-variadic dispatcher code.

--- src/ovxx/dispatcher/diagnostics.hpp
+++ src/ovxx/dispatcher/diagnostics.hpp
@@ -141,9 +141,6 @@ struct backend_of<Evaluator<O, B, S, E> >
 template <typename E, typename S, bool V = E::ct_valid>
 struct traits;
 
-// If we can use variadic templates, life is easy...
-#if __GXX_EXPERIMENTAL_CXX0X__
-
 template <typename E, typename ...A>
 struct traits<E, void(A...), true>
 {
@@ -157,97 +154,6 @@ struct traits<E, void(A...), false>
   static char const *name() { return backend_of<E>::name();}
   static bool rt_valid(A...) { return false;}
 };
-
-#else
-
-template <typename E, typename A>
-struct traits<E, void(A), true>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A a) { return E::rt_valid(a);}
-};
-
-template <typename E, typename A>
-struct traits<E, void(A), false>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A) { return false;}
-};
-
-template <typename E, typename A1, typename A2>
-struct traits<E, void(A1, A2), true>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1 a1, A2 a2) { return E::rt_valid(a1, a2);}
-};
-
-template <typename E, typename A1, typename A2>
-struct traits<E, void(A1, A2), false>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1, A2) { return false;}
-};
-
-template <typename E, typename A1, typename A2, typename A3>
-struct traits<E, void(A1, A2, A3), true>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1 a1, A2 a2, A3 a3) { return E::rt_valid(a1, a2, a3);}
-};
-
-template <typename E, typename A1, typename A2, typename A3>
-struct traits<E, void(A1, A2, A3), false>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1, A2, A3) { return false;}
-};
-
-template <typename E, typename A1, typename A2, typename A3, typename A4>
-struct traits<E, void(A1, A2, A3, A4), true>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1 a1, A2 a2, A3 a3, A4 a4)
-  { return E::rt_valid(a1, a2, a3, a4);}
-};
-
-template <typename E, typename A1, typename A2, typename A3, typename A4>
-struct traits<E, void(A1, A2, A3, A4), false>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1, A2, A3, A4) { return false;}
-};
-
-template <typename E, typename A1, typename A2, typename A3, typename A4, typename A5>
-struct traits<E, void(A1, A2, A3, A4, A5), true>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-  { return E::rt_valid(a1, a2, a3, a4, a5);}
-};
-
-template <typename E, typename A1, typename A2, typename A3, typename A4, typename A5>
-struct traits<E, void(A1, A2, A3, A4, A5), false>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1, A2, A3, A4, A5) { return false;}
-};
-
-template <typename E, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
-struct traits<E, void(A1, A2, A3, A4, A5, A6), true>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-  { return E::rt_valid(a1, a2, a3, a4, a5, a6);}
-};
-
-template <typename E, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
-struct traits<E, void(A1, A2, A3, A4, A5, A6), false>
-{
-  static char const *name() { return backend_of<E>::name();}
-  static bool rt_valid(A1, A2, A3, A4, A5, A6) { return false;}
-};
-
-#endif
 
 template <typename E, typename S>
 std::string backend_diag(bool rt_valid)
@@ -263,9 +169,6 @@ std::string backend_diag(bool rt_valid)
 }
 } // namespace ovxx::dispatcher::detail
 
-// If we can use variadic templates, life is easy...
-#if __GXX_EXPERIMENTAL_CXX0X__
-
 template <typename E, typename ...A>
 std::string backend_diag(A... args)
 {
@@ -273,60 +176,6 @@ std::string backend_diag(A... args)
   bool rt_valid = traits::rt_valid(args...);
   return detail::backend_diag<E, void(A...)>(rt_valid);
 }
-
-#else
-
-template <typename E, typename A>
-std::string backend_diag(A a)
-{
-  typedef detail::traits<E, void(A)> traits;
-  bool rt_valid = traits::rt_valid(a);
-  return detail::backend_diag<E, void(A)>(rt_valid);
-}
-
-template <typename E, typename A1, typename A2>
-std::string backend_diag(A1 a1, A2 a2)
-{
-  typedef detail::traits<E, void(A1, A2)> traits;
-  bool rt_valid = traits::rt_valid(a1, a2);
-  return detail::backend_diag<E, void(A1, A2)>(rt_valid);
-}
-
-template <typename E, typename A1, typename A2, typename A3>
-std::string backend_diag(A1 a1, A2 a2, A3 a3)
-{
-  typedef detail::traits<E, void(A1, A2, A3)> traits;
-  bool rt_valid = traits::rt_valid(a1, a2, a3);
-  return detail::backend_diag<E, void(A1, A2, A3)>(rt_valid);
-}
-
-template <typename E, typename A1, typename A2, typename A3, typename A4>
-std::string backend_diag(A1 a1, A2 a2, A3 a3, A4 a4)
-{
-  typedef detail::traits<E, void(A1, A2, A3, A4)> traits;
-  bool rt_valid = traits::rt_valid(a1, a2, a3, a4);
-  return detail::backend_diag<E, void(A1, A2, A3, A4)>(rt_valid);
-}
-
-template <typename B, typename E,
-	  typename A1, typename A2, typename A3, typename A4, typename A5>
-std::string backend_diag(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-{
-  typedef detail::traits<E, void(A1, A2, A3, A4, A5)> traits;
-  bool rt_valid = traits::rt_valid(a1, a2, a3, a4, a5);
-  return detail::backend_diag<E, void(A1, A2, A3, A4, A5)>(rt_valid);
-}
-
-template <typename B, typename E,
-	  typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
-std::string backend_diag(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-{
-  typedef detail::traits<E, void(A1, A2, A3, A4, A5, A6)> traits;
-  bool rt_valid = traits::rt_valid(a1, a2, a3, a4, a5, a6);
-  return detail::backend_diag<E, void(A1, A2, A3, A4, A5, A6)>(rt_valid);
-}
-
-#endif
 
 /// Do the same traversal as the Dispatcher, but don't skip ct_valid=false
 template <typename O,                               // operation
@@ -349,9 +198,6 @@ struct Diagnostics<O, T, L, B, null_type, E>
   static bool const ct_valid = E::ct_valid;
 };
 
-// If we can use variadic templates, life is easy...
-#if __GXX_EXPERIMENTAL_CXX0X__
-
 template <typename O, typename L, typename B, typename N, typename E, typename R, typename ...A>
 struct Diagnostics<O, R(A...), L, B, N, E>
 {
@@ -372,178 +218,11 @@ struct Diagnostics<O, R(A...), L, B, null_type, E>
   static std::string diag(A... args) { return backend_diag<E, A...>(args...);}
 };
 
-#else
-
-/// General case for R(A).
-template <typename O,
-          typename R, typename A,
-          typename L, typename B, typename N, typename E>
-struct Diagnostics<O, R(A), L, B, N, E>
-{
-  static bool const ct_valid = E::ct_valid || Diagnostics<O, R(A), N>::ct_valid;
-  static std::string diag(A a)
-  {
-    std::string msg = backend_diag<E, A>(a);
-    msg += Diagnostics<O, R(A), N>::diag(a);
-    return msg;
-  }
-};
-
-/// Terminator for R(A)
-template <typename O, typename R, typename A, typename L, typename B, typename E>
-struct Diagnostics<O, R(A), L, B, null_type, E>
-{
-  static bool const ct_valid = E::ct_valid;
-  static std::string diag(A a) { return backend_diag<E, A>(a);}
-};
-
-/// General case for R(A1, A2).
-template <typename O,
-          typename R, typename A1, typename A2,
-          typename L, typename B, typename N, typename E>
-struct Diagnostics<O, R(A1, A2), L, B, N, E>
-{
-  static bool const ct_valid = E::ct_valid ||
-    Diagnostics<O, R(A1, A2), N>::ct_valid;
-  static std::string diag(A1 a1, A2 a2)
-  {
-    std::string msg = backend_diag<E, A1, A2>(a1, a2);
-    msg += Diagnostics<O, R(A1, A2), N>::diag(a1, a2);
-    return msg;
-  }
-};
-
-/// Terminator for R(A1, A2)
-template <typename O,
-          typename R, typename A1, typename A2,
-          typename L, typename B, typename E>
-struct Diagnostics<O, R(A1, A2), L, B, null_type, E>
-{
-  static bool const ct_valid = E::ct_valid;
-  static std::string diag(A1 a1, A2 a2) { return backend_diag<E, A1, A2>(a1, a2);}
-};
-
-/// General case for R(A1, A2, A3).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3,
-          typename L, typename B, typename N, typename E>
-struct Diagnostics<O, R(A1, A2, A3), L, B, N, E>
-{
-  static bool const ct_valid = E::ct_valid ||
-    Diagnostics<O, R(A1, A2, A3), N>::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3)
-  {
-    std::string msg = backend_diag<E, A1, A2, A3>(a1, a2, a3);
-    msg += Diagnostics<O, R(A1, A2, A3), N>::diag(a1, a2, a3);
-    return msg;
-  }
-};
-
-/// Terminator for R(A1, A2, A3).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3,
-          typename L, typename B, typename E>
-struct Diagnostics<O, R(A1, A2, A3), L, B, null_type, E>
-{
-  static bool const ct_valid = E::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3) { return backend_diag<E, A1, A2, A3>(a1, a2, a3);}
-};
-
-/// General case for R(A1, A2, A3, A4).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename L, typename B, typename N, typename E>
-struct Diagnostics<O, R(A1, A2, A3, A4), L, B, N, E>
-{
-  static bool const ct_valid = E::ct_valid ||
-    Diagnostics<O, R(A1, A2, A3, A4), N>::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3, A4 a4)
-  {
-    std::string msg = backend_diag<E, A1, A2, A3, A4>(a1, a2, a3, a4);
-    msg += Diagnostics<O, R(A1, A2, A3, A4), N>::diag(a1, a2, a3, a4);
-    return msg;
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename L, typename B, typename E>
-struct Diagnostics<O, R(A1, A2, A3, A4), L, B, null_type, E>
-{
-  static bool const ct_valid = E::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3, A4 a4)
-  { return backend_diag<E, A1, A2, A3, A4>(a1, a2, a3, a4);}
-};
-
-/// General case for R(A1, A2, A3, A4, A5).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5,
-          typename L, typename B, typename N, typename E>
-struct Diagnostics<O, R(A1, A2, A3, A4, A5), L, B, N, E>
-{
-  static bool const ct_valid = E::ct_valid ||
-    Diagnostics<O, R(A1, A2, A3, A4, A5), N>::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-  {
-    std::string msg = backend_diag<E, A1, A2, A3, A4, A5>(a1, a2, a3, a4, a5);
-    msg += Diagnostics<O, R(A1, A2, A3, A4, A5), N>::diag(a1, a2, a3, a4, a5);
-    return msg;
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4, A5).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5,
-          typename L, typename B, typename E>
-struct Diagnostics<O, R(A1, A2, A3, A4, A5), L, B, null_type, E>
-{
-  static bool const ct_valid = E::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-  { return backend_diag<E, A1, A2, A3, A4, A5>(a1, a2, a3, a4, a5);}
-};
-
-/// General case for R(A1, A2, A3, A4, A5, A6).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5, typename A6,
-          typename L, typename B, typename N, typename E>
-struct Diagnostics<O, R(A1, A2, A3, A4, A5, A6), L, B, N, E>
-{
-  static bool const ct_valid = E::ct_valid ||
-    Diagnostics<O, R(A1, A2, A3, A4, A5, A6), N>::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-  {
-    std::string msg = backend_diag<E, A1, A2, A3, A4, A5, A6>(a1, a2, a3, a4, a5, a6);
-    msg += Diagnostics<O, R(A1, A2, A3, A4, A5, A6), N>::diag(a1, a2, a3, a4, a5, a6);
-    return msg;
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4, A5, A6).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5, typename A6,
-          typename L, typename B, typename E>
-struct Diagnostics<O, R(A1, A2, A3, A4, A5, A6), L, B, null_type, E>
-{
-  static bool const ct_valid = E::ct_valid;
-  static std::string diag(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-  { return backend_diag<E, A1, A2, A3, A4, A5, A6>(a1, a2, a3, a4, a5, a6);}
-};
-
-#endif
-
 template <typename O, typename S>
 struct is_operation_supported
 {
   static bool const value = Diagnostics<O, S>::ct_valid;
 };
-
-// If we can use variadic templates, life is easy...
-#if __GXX_EXPERIMENTAL_CXX0X__
 
 /// print dispatch diagnostics for a given operation.
 /// Template parameters:
@@ -560,62 +239,6 @@ std::string diagnostics(A... args)
   typedef Diagnostics<O, R(A...)> d;
   return d::diag(args...);
 }
-
-#else
-
-/// print dispatch diagnostics for a given operation.
-/// Template parameters:
-///   :O: The operation (tag) to diagnose.
-///   :R: The return type.
-///   :A: The argument type.
-///
-/// Where normally dispatch<O>(a) would be called to perform
-/// an operation, dispatch_diagnostics<O>(a) can be used to
-/// print out diagnostics without actually performing it.
-template <typename O, typename R, typename A>
-std::string diagnostics(A a)
-{
-  typedef Diagnostics<O, R(A)> d;
-  return d::diag(a);
-}
-
-template <typename O, typename R, typename A1, typename A2>
-std::string diagnostics(A1 a1, A2 a2)
-{
-  typedef Diagnostics<O, R(A1, A2)> d;
-  return d::diag(a1, a2);
-}
-
-template <typename O, typename R, typename A1, typename A2, typename A3>
-std::string diagnostics(A1 a1, A2 a2, A3 a3)
-{
-  typedef Diagnostics<O, R(A1, A2, A3)> d;
-  return d::diag(a1, a2, a3);
-}
-
-template <typename O, typename R, typename A1, typename A2, typename A3, typename A4>
-std::string diagnostics(A1 a1, A2 a2, A3 a3, A4 a4)
-{
-  typedef Diagnostics<O, R(A1, A2, A3, A4)> d;
-  return d::diag(a1, a2, a3, a4);
-}
-
-template <typename O, typename R, typename A1, typename A2, typename A3, typename A4, typename A5>
-std::string diagnostics(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-{
-  typedef Diagnostics<O, R(A1, A2, A3, A4, A5)> d;
-  return d::diag(a1, a2, a3, a4, a5);
-}
-
-template <typename O, typename R, typename A1, typename A2, typename A3, typename A4,
-	  typename A5, typename A6>
-std::string diagnostics(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-{
-  typedef Diagnostics<O, R(A1, A2, A3, A4, A5, A6)> d;
-  return d::diag(a1, a2, a3, a4, a5, a6);
-}
-
-#endif
 
 } // namespace ovxx::dispatcher
 } // namespace ovxx
--- src/ovxx/dispatcher/dispatcher.hpp
+++ src/ovxx/dispatcher/dispatcher.hpp
@@ -9,7 +9,6 @@
 #define ovxx_dispatcher_dispatcher_hpp_
 
 #include <ovxx/support.hpp>
-#include <ovxx/c++11.hpp>
 
 namespace ovxx
 {
@@ -24,45 +23,11 @@ struct type_list
   typedef T tail;
 };
 
-// If we can use variadic templates, life is easy...
-#if __GXX_EXPERIMENTAL_CXX0X__
-
 template <typename H = null_type, typename...T>
 struct make_type_list
 {
   typedef type_list<H, typename make_type_list<T...>::type> type;
 };
-
-#else
-
-template <typename T1 = null_type,
-	  typename T2 = null_type,
-	  typename T3 = null_type,
-	  typename T4 = null_type,
-	  typename T5 = null_type,
-	  typename T6 = null_type,
-	  typename T7 = null_type,
-	  typename T8 = null_type,
-	  typename T9 = null_type,
-	  typename T10 = null_type,
-	  typename T11 = null_type,
-	  typename T12 = null_type,
-	  typename T13 = null_type,
-	  typename T14 = null_type,
-	  typename T15 = null_type,
-	  typename T16 = null_type,
-	  typename T17 = null_type>
-struct make_type_list
-{
-private:
-  typedef typename 
-  make_type_list<T2, T3, T4, T5, T6, T7, T8, T9, T10,
-		 T11, T12, T13, T14, T15, T16, T17>::type Rest;
-public:
-  typedef type_list<T1, Rest> type;
-};
-
-#endif
 
 template<>
 struct make_type_list<>
@@ -127,9 +92,6 @@ struct Dispatcher<O, T, L, B, N, E, true>
   typedef typename E::backend_type type;
 };
 
-// If we can use variadic templates, life is easy...
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-
 /// General case for R(A...).
 template <typename O,
           typename L, typename B, typename N, typename E,
@@ -166,255 +128,6 @@ struct Dispatcher<O, R(A... args), L, B, null_type, E, false>
     throw std::runtime_error("No backend");
   }
 };
-
-#else
-
-/// General case for R(A).
-template <typename O,
-          typename R, typename A,
-          typename L, typename B, typename N, typename E>
-struct Dispatcher<O, R(A), L, B, N, E, true>
-{
-  typedef B backend;
-  static R dispatch(A a)
-  {
-    if (E::rt_valid(a)) return E::exec(a);
-    else return Dispatcher<O, R(A), N>::dispatch(a);
-  }
-};
-
-/// Terminator for R(A): If rt-check fails, give up.
-template <typename O, typename R, typename A, typename L, typename B, typename E>
-struct Dispatcher<O, R(A), L, B, null_type, E, true>
-{
-  typedef B backend;
-  static R dispatch(A a)
-  {
-    if (E::rt_valid(a))
-      return E::exec(a);
-    else OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// Terminator for R(A): Give up.
-template <typename O, typename R, typename A, typename L, typename B, typename E>
-struct Dispatcher<O, R(A), L, B, null_type, E, false>
-{
-  static R dispatch(A)
-  {
-    OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// General case for R(A1, A2).
-template <typename O,
-          typename R, typename A1, typename A2,
-          typename L, typename B, typename N, typename E>
-struct Dispatcher<O, R(A1, A2), L, B, N, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2)
-  {
-    if (E::rt_valid(a1, a2)) return E::exec(a1, a2);
-    else return Dispatcher<O, R(A1, A2), N>::dispatch(a1, a2);
-  }
-};
-
-/// Terminator for R(A1, A2): If rt-check fails, give up.
-template <typename O,
-          typename R, typename A1, typename A2,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2), L, B, null_type, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2)
-  {
-    if (E::rt_valid(a1, a2)) return E::exec(a1, a2);
-    else OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// Terminator for R(A1, A2): Give up.
-template <typename O,
-          typename R, typename A1, typename A2,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2), L, B, null_type, E, false>
-{
-  static R dispatch(A1, A2)
-  {
-    OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// General case for R(A1, A2, A3).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3,
-          typename L, typename B, typename N, typename E>
-struct Dispatcher<O, R(A1, A2, A3), L, B, N, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3)
-  {
-    if (E::rt_valid(a1, a2, a3)) return E::exec(a1, a2, a3);
-    else return Dispatcher<O, R(A1, A2, A3), N>::dispatch(a1, a2, a3);
-  }
-};
-
-/// Terminator for R(A1, A2, A3): If rt-check fails, give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3), L, B, null_type, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3)
-  {
-    if (E::rt_valid(a1, a2, a3)) return E::exec(a1, a2, a3);
-    else OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// Terminator for R(A1, A2, A3): Give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3), L, B, null_type, E, false>
-{
-  static R dispatch(A1, A2, A3)
-  {
-    OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// General case for R(A1, A2, A3, A4).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename L, typename B, typename N, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4), L, B, N, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3, A4 a4)
-  {
-    if (E::rt_valid(a1, a2, a3, a4)) return E::exec(a1, a2, a3, a4);
-    else return Dispatcher<O, R(A1, A2, A3, A4), N>::dispatch(a1, a2, a3, a4);
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4): If rt-check fails, give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4), L, B, null_type, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3, A4 a4)
-  {
-    if (E::rt_valid(a1, a2, a3, a4)) return E::exec(a1, a2, a3, a4);
-    else OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4): Give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4), L, B, null_type, E, false>
-{
-  static R dispatch(A1, A2, A3, A4)
-  {
-    OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// General case for R(A1, A2, A3, A4, A5).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5,
-          typename L, typename B, typename N, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4, A5), L, B, N, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-  {
-    if (E::rt_valid(a1, a2, a3, a4, a5)) return E::exec(a1, a2, a3, a4, a5);
-    else return Dispatcher<O, R(A1, A2, A3, A4, A5), N>::dispatch
-      (a1, a2, a3, a4, a5);
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4, A5): If rt-check fails, give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4, A5), L, B, null_type, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5)
-  {
-    if (E::rt_valid(a1, a2, a3, a4, a5)) return E::exec(a1, a2, a3, a4, a5);
-    else OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4, A5): Give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4, A5), L, B, null_type, E, false>
-{
-  static R dispatch(A1, A2, A3, A4, A5)
-  {
-    OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// General case for R(A1, A2, A3, A4, A5, A6).
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5, typename A6,
-          typename L, typename B, typename N, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4, A5, A6), L, B, N, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-  {
-    if (E::rt_valid(a1, a2, a3, a4, a5, a6)) return E::exec(a1, a2, a3, a4, a5, a6);
-    else return Dispatcher<O, R(A1, A2, A3, A4, A5, A6), N>::dispatch
-      (a1, a2, a3, a4, a5, a6);
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4, A5, A6): If rt-check fails, give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5, typename A6,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4, A5, A6), L, B, null_type, E, true>
-{
-  typedef B backend;
-  static R dispatch(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6)
-  {
-    if (E::rt_valid(a1, a2, a3, a4, a5, a6)) return E::exec(a1, a2, a3, a4, a5, a6);
-    else OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-/// Terminator for R(A1, A2, A3, A4, A5, A6): Give up.
-template <typename O,
-          typename R, typename A1, typename A2, typename A3, typename A4,
-          typename A5, typename A6,
-          typename L, typename B, typename E>
-struct Dispatcher<O, R(A1, A2, A3, A4, A5, A6), L, B, null_type, E, false>
-{
-  static R dispatch(A1, A2, A3, A4, A5, A6)
-  {
-    OVXX_DO_THROW(unimplemented("No backend"));
-  }
-};
-
-#endif
 
 } // namespace ovxx::dispatcher
 } // namespace ovxx
